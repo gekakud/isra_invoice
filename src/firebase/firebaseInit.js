578ab6af958d7828e30cbe2396992284f9ff1cc1
geka <geka.kud@gmail.com>
2021-10-07 20:18:09 +0300

    add firebase

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBtX0zbq8OWFoczz-D55IEQ-zQxcmjZ6BU",
-  authDomain: "invoice-app-yt.firebaseapp.com",
-  projectId: "invoice-app-yt",
-  storageBucket: "invoice-app-yt.appspot.com",
-  messagingSenderId: "638958069840",
-  appId: "1:638958069840:web:2a359ba0cf4b8c7b648955",
+  apiKey: "AIzaSyAx3Qfp2YjXk7wcwt36cVyaNav0aRk_lzE",
+  authDomain: "invoice-213d7.firebaseapp.com",
+  projectId: "invoice-213d7",
+  storageBucket: "invoice-213d7.appspot.com",
+  messagingSenderId: "628731390469",
+  appId: "1:628731390469:web:f0e52961cca886dabf8a6c"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
